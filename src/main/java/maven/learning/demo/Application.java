12f9ae3c4143aadf0d56a2e545a13106f59b8045
Shruti Solani <shrutiusolani@gmail.com>
2023-07-31 21:16:31 +0530

    added dependency

--- src/main/java/maven/learning/demo/Application.java
+++ src/main/java/maven/learning/demo/Application.java
@@ -2,9 +2,15 @@ package maven.learning.demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 
 public class Application {
+
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
 
     public void greet() {
         List<String> greetings = new ArrayList<>();
@@ -25,5 +31,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
         app.greet();
+        int count = app.countWords("Here are a few words");
+        System.out.println("Word count: " + count);
     }
 }
